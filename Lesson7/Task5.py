6e7151644d7853aecf4d627dc620c75cbfab0c1d
Sergey Gukov <svgukoff@mail.ru>
2023-03-05 18:06:36 +0300

    add

--- Lesson7/Task5.py
+++ Lesson7/Task5.py
@@ -18,9 +18,10 @@ alpha = 0.05
 x_len = len(x)
 x_mean = np.mean(x)
 x_sigma = x.std(ddof=1)
-t = stats.t.ppf(1 - alpha / 2, df = x_len - 1)
+t = stats.t.ppf(1 - alpha / 2, df=x_len - 1)
 result = testing_the_hypothesis(mu, x_mean, x_len, x_sigma)
 print(f"Observable: {result}")
 print(f"critical t: {t}")
 
 # t не принадлежит критической области, гипотеза H0 не отвергается на уровне значимости 0.05
+
